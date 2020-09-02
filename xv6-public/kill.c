93cfc2559ebcc0b7d5cfc436b8dad37dec18ebf3
UlisesBojorquez <ulibojorquez@gmail.com>
2020-09-02 01:26:03 +0000

    practica 2

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -12,6 +12,10 @@ main(int argc, char **argv)
     exit();
   }
   for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+    if(atoi(argv[i])==1){
+        printf(1, "No puedes borrar el init...\n");
+    }else{
+        kill(atoi(argv[i]));
+    }
   exit();
 }
